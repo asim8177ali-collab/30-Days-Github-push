23f6cdf7a1c9092436b0acf84e29025b5199026d
Asim Bharwana <asim8177ali@gmail.com>
2026-03-17 12:27:43 +0500

    day-1 done

---
+++ Day-1.py
@@ -0,0 +1,8 @@
+name= input("Enter your name: ")
+# print (f"Hello {name}! How are you?")  # for direct printing 
+
+def greeting(name):                      # function for greeting 
+    return f"Hello {name}! How are you?"
+
+print(greeting(name))
+
